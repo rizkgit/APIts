d9ad731a55e01382a99573504651c0679b185ec3
Rony Rizk <rony_rizk@live.com>
2017-11-10 09:24:52 +0100

    c5

--- BLC.js
+++ BLC.js
@@ -25,6 +25,26 @@ module.exports = {
     // --------------------------------------------
 
     // --------------------------------------------
+    getProductImages : function(API_URL,PRODUCT_ID,PAGE){
+        return new Promise(function(resolve,reject){
+            var file = './data/ProductImages.json'
+            jsonfile.readFile(file, function(err, obj) {
+              
+              if (obj != null){
+                obj = obj.filter(function(x){return x.ID == PRODUCT_ID});
+              }          
+              var toRet = obj.slice((PAGE - 1) * page_size, PAGE * page_size);
+              if (toRet != null){
+                  toRet.forEach((e)=>{
+                      e.IMG_URL = API_URL + '/Images/ProductImages/' + e.IMG_URL;
+                  })
+              }
+             resolve(toRet);
+        });
+    }
+    // --------------------------------------------
+
+    // --------------------------------------------
     test: function(){
         return new Promise(function(resolve,reject){
             resolve(true);
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json())
 // use it before all route definitions
 // ------------
 var cors = require('cors');
-app.use(cors({origin: 'http://localhost:8100'}));
+app.use(cors({ origin: 'http://localhost:8100' }));
 // ------------
 
 // Json File 
@@ -29,78 +29,86 @@ var jsonfile = require('jsonfile')
 
 // Actions
 // ------------
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
 	res.send('Oops: Nothing to show till now');
 })
 
 
-app.get('/FeaturedProducts',function(req,res){
+app.get('/FeaturedProducts', function (req, res) {
+
 	var file = './data/Products.json'
-	jsonfile.readFile(file, function(err, obj) {	   
-	   var toRet = [];
-	   obj.forEach((e) =>{
-		   if (e.IS_FEATURED == true) {
-			   toRet.push(e)
+	jsonfile.readFile(file, function (err, obj) {
+		var toRet = [];
+		obj.forEach((e) => {
+			if (e.IS_FEATURED == true) {
+				toRet.push(e)
 			};
-		  })
-	  if (toRet != null){
-		  toRet.forEach((e)=>{
-			  e.IMG_URL = API_URL + '/Images/Products/' + e.IMG_URL;
-		  })
-	  }
-	   res.send(toRet);
+		})
+		if (toRet != null) {
+			toRet.forEach((e) => {
+				e.IMG_URL = API_URL + '/Images/Products/' + e.IMG_URL;
+			})
+		}
+		res.send(toRet);
 	})
 })
 
-app.get('/RootCategories',function(req,res){
+app.get('/RootCategories', function (req, res) {
 	var file = './data/Categories.json'
-	jsonfile.readFile(file, function(err, obj) {
-	  var toRet = [];
-	  obj.forEach((e)=>{
-		  if (e.PARENT_ID == "0"){
-			  toRet.push(e);
-		  }
-	  });
-	  if (toRet != null){
-		  toRet.forEach((e)=>{
-			  e.IMG_URL = API_URL + '/Images/Categories/' + e.IMG_URL;
-		  })
-	  }
-	  res.send(toRet);
+	jsonfile.readFile(file, function (err, obj) {
+		var toRet = [];
+		obj.forEach((e) => {
+			if (e.PARENT_ID == "0") {
+				toRet.push(e);
+			}
+		});
+		if (toRet != null) {
+			toRet.forEach((e) => {
+				e.IMG_URL = API_URL + '/Images/Categories/' + e.IMG_URL;
+			})
+		}
+		res.send(toRet);
 	})
 })
 
-app.get('/Categories',function(req,res){
+app.get('/Categories', function (req, res) {
 	var file = './data/Categories.json'
-	jsonfile.readFile(file, function(err, obj) {
-	  res.send(obj);
+	jsonfile.readFile(file, function (err, obj) {
+		res.send(obj);
 	})
 })
 
-app.post('/Products',function(req,res){
-	
+app.post('/Products', function (req, res) {
+
 	var page_size = 5;
-	var page_number = req.body.page;	
+	var page_number = req.body.page;
 	var file = './data/Products.json'
-	jsonfile.readFile(file, function(err, obj) {
-	  var toRet = obj.slice((page_number - 1) * page_size, page_number * page_size);
-	  if (toRet != null){
-		  toRet.forEach((e)=>{
-			  e.IMG_URL = API_URL + '/Images/Products/' + e.IMG_URL;
-		  })
-	  }
-	  res.send(toRet);
+	jsonfile.readFile(file, function (err, obj) {
+		var toRet = obj.slice((page_number - 1) * page_size, page_number * page_size);
+		if (toRet != null) {
+			toRet.forEach((e) => {
+				e.IMG_URL = API_URL + '/Images/Products/' + e.IMG_URL;
+			})
+		}
+		res.send(toRet);
 	})
 })
 
-app.post('/CategoryProducts',function(req,res){	
-	var CATEGORY_ID = req.body.CATEGORY_ID;	
+app.post('/CategoryProducts', function (req, res) {
+	var CATEGORY_ID = req.body.CATEGORY_ID;
 	var page_number = req.body.page;
-	var file  = './data/Products.json'
 
-	blc.getCategoryProducts(API_URL,CATEGORY_ID,page_number)
-	    .then((data) => res.send(data));
+	blc.getCategoryProducts(API_URL, CATEGORY_ID, page_number)
+		.then((data) => res.send(data));
 
+})
+
+app.post('/ProductImages', function (req, res) {
+	var PRODUCT_ID = req.body.PRODUCT_ID;
+	var page_number = req.body.page;
+
+	blc.getProductImages(API_URL, PRODUCT_ID, page_number)
+		.then((data) => res.send(data));
 })
 // ------------
 
@@ -109,31 +117,31 @@ app.post('/CategoryProducts',function(req,res){
 var dir = path.join(__dirname, '');
 
 var mime = {
-    html: 'text/html',
-    txt: 'text/plain',
-    css: 'text/css',
-    gif: 'image/gif',
-    jpg: 'image/jpeg',
-    png: 'image/png',
-    svg: 'image/svg+xml',
-    js: 'application/javascript'
+	html: 'text/html',
+	txt: 'text/plain',
+	css: 'text/css',
+	gif: 'image/gif',
+	jpg: 'image/jpeg',
+	png: 'image/png',
+	svg: 'image/svg+xml',
+	js: 'application/javascript'
 };
 
 app.get('*', function (req, res) {
-    var file = path.join(dir, req.path.replace(/\/$/, '/index.html'));
-    if (file.indexOf(dir + path.sep) !== 0) {
-        return res.status(403).end('Forbidden');
-    }
-    var type = mime[path.extname(file).slice(1)] || 'text/plain';
-    var s = fs.createReadStream(file);
-    s.on('open', function () {
-        res.set('Content-Type', type);
-        s.pipe(res);
-    });
-    s.on('error', function () {
-        res.set('Content-Type', 'text/plain');
-        res.status(404).end('Not found');
-    });
+	var file = path.join(dir, req.path.replace(/\/$/, '/index.html'));
+	if (file.indexOf(dir + path.sep) !== 0) {
+		return res.status(403).end('Forbidden');
+	}
+	var type = mime[path.extname(file).slice(1)] || 'text/plain';
+	var s = fs.createReadStream(file);
+	s.on('open', function () {
+		res.set('Content-Type', type);
+		s.pipe(res);
+	});
+	s.on('error', function () {
+		res.set('Content-Type', 'text/plain');
+		res.status(404).end('Not found');
+	});
 });
 // ------------
 
@@ -145,7 +153,7 @@ const publicIp = require('public-ip');
 var API_URL = '';
 
 publicIp.v4().then(ip => {
-	API_URL	= 'http://' + ip + ':' + port;
+	API_URL = 'http://' + ip + ':' + port;
 	app.listen(port);
 	console.log("API is listening on " + "[" + API_URL + "]");
 });
